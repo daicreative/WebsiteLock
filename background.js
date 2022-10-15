832034955a4648940437d20060e8eec8ecd8a182
Imon Husain <imonh222@gmail.com>
2022-10-14 23:17:32 -0700

    refresher update

--- background.js
+++ background.js
@@ -1,40 +1,54 @@
 skipURL = "";
 
-initializeDefaultValues();
+chrome.runtime.onInstalled.addListener(function (details) {
+	initializeDefaultValues();
+});
 
 function initializeDefaultValues() {
-	chrome.storage.local.get('domains', function(domains){
-		if(domains == undefined) chrome.storage.local.set({'domains': {}});
+	chrome.storage.local.get(function (data) {
+		if (data.domains == undefined) {
+			data.domains = {}
+			chrome.storage.local.set(data);
+		}
 	});
 }
 
 chrome.tabs.onUpdated.addListener(function (tabId, changedInfo, tab) {
-	if(tab.url == skipURL) return;
-	else skipURL = "";
 
-	var url = new URL(tab.url);
-	var hostname = url.hostname.replace("www.", "");
-	
-	chrome.storage.local.get(function (data) {
-		// Check if hostname is a checked domain
-		var domain = data.domains[hostname];
-		if (domain != undefined) {
-			var block = false;
-			for (var i = 0; i < domain.regexes.length; i++) {
-				var regex = new RegExp(domain.regexes[i]);
+	if (changedInfo.status == "loading") {
 
-				if (regex.test(url.pathname)) {
-					block = true;
-					break;
+		if (trim(tab.url) == trim(skipURL)) return;
+		skipURL = "";
+
+		var url = new URL(tab.url);
+		var hostname = url.hostname.replace("www.", "");
+
+
+		chrome.storage.local.get(function (data) {
+			// Check if hostname is a checked domain
+
+			var domain = data.domains[hostname];
+			if (domain != undefined) {
+				var block = false;
+				for (var i = 0; i < domain.regexes.length; i++) {
+					var regex = new RegExp(domain.regexes[i]);
+
+					if (regex.test(url.pathname)) {
+						block = true;
+						break;
+					}
+				}
+
+				if (block) {
+					chrome.tabs.update(tabId, {
+						"url": "blocked.html?redirect=" + tab.url
+					});
 				}
 			}
-
-			if (block) {
-				chrome.tabs.update(tabId, {
-					"url": "blocked.html?redirect=" + tab.url
-				});
-			}
-		}
-	});
-
+		});
+	} 
 });
+
+function trim(s) {
+	return s.replace(/\/+$/, "");
+}
